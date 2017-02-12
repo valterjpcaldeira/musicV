74a343faeeb5031f08ff40a312bdef5f789c28d0
Valter Caldeira <valterjpcaldeira@gmail.com>
2017-02-12 18:01:23 +0000

    using nodejs like a pro

---
+++ webapp/config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '1209604419094908', // your App ID
+        'clientSecret'  : 'a050fed61c6d66ddb7e47a87321b816b', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '491570814882-etbh3sch3uvfraqc7c0platlibdvuskt.apps.googleusercontent.com',
+        'clientSecret'  : 'mn-VEyLUc-DWVbwd7zN9NBTB',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
--- webapp/indexController.js
+++ webapp/indexController.js
@@ -4,22 +4,40 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var wedeployMiddleware = require('wedeploy-middleware');
 var WeDeploy = require('wedeploy');
+var passport = require('passport');
+var flash    = require('connect-flash');
 var app = express();
 
-//Static files
-app.use(express.static('public'));
-app.use(express.static('private'));
+//CONFIG
+require('./config/passport')(passport); // pass passport for configuration
 
-app.use(morgan('combined'));
+app.configure(function() {
+
+	// set up our express application
+	app.use(express.logger('dev')); // log every request to the console
+	app.use(express.cookieParser()); // read cookies (needed for auth)
+
+	// required for passport
+	app.use(express.session({ secret: 'ilovewedeploywedeploywedeploy' })); // session secret
+	app.use(passport.initialize());
+	app.use(passport.session()); // persistent login sessions
+	app.use(flash()); // use connect-flash for flash messages stored in session
+
+	app.use(function(err, req, res, next) {
+	    console.log(err);
+	    res.send(500);
+	});
+
+	app.use(favicon(__dirname + '/public/images/like.ico'));
+
+});
+
+
+//app.use(morgan('combined'));
 
 //ERROR HANdler
 //express error handler (never called)
-app.use(function(err, req, res, next) {
-    console.log(err);
-    res.send(500);
-});
 
-app.use(favicon(__dirname + '/public/images/like.ico'));
 
 
 /////////PUBLIC
@@ -47,13 +65,43 @@ app.get('/search/:tagId', function (req, res) {
 
 });
 
-/////////////////PRIVATE
-app.use(wedeployMiddleware.auth({url: 'auth.musicv.wedeploy.io',redirect: '/login'}));
-
-app.get('/', function (req, res, next) {
+app.get('/', isLoggedIn, function (req, res, next) {
     console.log('User: ', res.locals.user);
 	res.sendFile(path.join(__dirname + '/private/index.html'));
 });
+
+	// =====================================
+	// FACEBOOK ROUTES =====================
+	// =====================================
+	// route for facebook authentication and login
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+	// handle the callback after facebook has authenticated the user
+	app.get('/auth/facebook/callback',
+		passport.authenticate('facebook', {
+			successRedirect : '/profile',
+			failureRedirect : '/'
+		}));
+
+	// =====================================
+	// LOGOUT ==============================
+	// =====================================
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+
+// route middleware to make sure
+function isLoggedIn(req, res, next) {
+
+	// if user is authenticated in the session, carry on
+	if (req.isAuthenticated())
+		return next();
+
+	// if they aren't redirect them to the home page
+	res.redirect('/login');
+}
 
 
 
