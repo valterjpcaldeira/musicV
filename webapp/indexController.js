68192538293d9b3a68d0fb2e1fec8358b9b122e4
Valter Caldeira <valterjpcaldeira@gmail.com>
2017-05-02 17:26:39 +0100

    Update indexController.js

--- webapp/indexController.js
+++ webapp/indexController.js
@@ -9,6 +9,17 @@ var app = express();
 
 //app.use(morgan('combined'));
 
+// Lista de Utilizadores
+var users = [
+  { id: 1, username: 'Pedro Pinto', email: 'ppinto@ppl.com' },
+  { id: 2, username: 'Francisco', email: 'francisco@ppl.com' }, 
+  { id: 3, username: 'Carla Figueiredo', email: 'cfig@ppl.com' }
+];
+// Definir um endpoint da API
+app.get('/api/listaUsers', function(req, res, next) {
+  res.send(users);
+})
+
 //CONFIG
 app.use(express.static('public'));
 app.use(function(err, req, res, next) {
