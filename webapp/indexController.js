d138eebfd445c06c5fb9cfaa8d4d5f0b9e64d1f9
valtercaldeira <valterjpcaldeira@gmail.com>
2017-05-05 09:23:59 +0100

    change to 80

--- webapp/indexController.js
+++ webapp/indexController.js
@@ -54,7 +54,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
